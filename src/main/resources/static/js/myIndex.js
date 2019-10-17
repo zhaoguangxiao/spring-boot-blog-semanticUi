3ab1d04492783bff9e3a044184e314efba8b95eb
Administrator <1764773283@qq.com>
2019-10-17 16:47:12 +0800

    列表增加回到顶部

--- src/main/resources/static/js/myIndex.js
+++ src/main/resources/static/js/myIndex.js
@@ -1,4 +1,23 @@
 // 移动端动态显示头部标签
 $('.menu.toggle').click(function () {
     $('.m-item').toggleClass('m-mobile-hide');
-})
+});
+
+
+// 返回顶部方法
+$('#toTop-div').click(function () {
+    $(window).scrollTo(0, 500);
+});
+
+
+// 滚动监听js事件
+var waypoint = new Waypoint({
+    element: document.getElementById('middle-content'),
+    handler: function (direction) {
+        if (direction == 'down') {
+            $('#toolbar-list').show(100);
+        } else {
+            $('#toolbar-list').hide(500);
+        }
+    }
+});
