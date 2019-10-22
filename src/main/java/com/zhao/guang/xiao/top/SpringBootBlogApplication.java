784392c4ea74971c7b0f2da8b2de6e479e8bb4d1
Administrator <1764773283@qq.com>
2019-10-22 21:04:33 +0800

    前端页面添加中文英文切换

--- src/main/java/com/zhao/guang/xiao/top/SpringBootBlogApplication.java
+++ src/main/java/com/zhao/guang/xiao/top/SpringBootBlogApplication.java
@@ -1,7 +1,10 @@
 package com.zhao.guang.xiao.top;
 
+import com.zhao.guang.xiao.top.config.MyLocaleResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
 
 /**
  * @author Administrator
@@ -11,6 +14,17 @@ public class SpringBootBlogApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootBlogApplication.class, args);
+    }
+
+
+    /**
+     * 引入国际化配置
+     *
+     * @return
+     */
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new MyLocaleResolver();
     }
 
 }
---
+++ src/main/java/com/zhao/guang/xiao/top/config/MyLocaleResolver.java
@@ -0,0 +1,28 @@
+package com.zhao.guang.xiao.top.config;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.LocaleResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+/**
+ * @author Administrator
+ * @version 1.0
+ * @date 2019/10/22 19:47
+ */
+public class MyLocaleResolver implements LocaleResolver {
+    @Override
+    public Locale resolveLocale(HttpServletRequest request) {
+        String l = request.getParameter("l");
+        Locale locale = Locale.getDefault();
+        if(!StringUtils.isEmpty(l)){
+            String[] split = l.split("_");
+            locale = new Locale(split[0],split[1]);
+        }
+        return locale;
+    }
+
+    @Override
+    public void setLocale(HttpServletRequest request, HttpServletResponse response, Locale locale) { }
+}
